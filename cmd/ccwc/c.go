9c10073c742eba5e0e5de79b6f6cbba0e6af35e7
qiang_liu <liuqiang9596@gmail.com>
2023-12-20 15:59:10 +0800

    output the number of bytes in a file

---
+++ cmd/ccwc/c.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/fs"
+	"os"
+	"strconv"
+)
+
+var cmd = &cobra.Command{}
+
+func init() {
+	rootCmd.AddCommand(cCmd)
+}
+
+var cCmd = &cobra.Command{
+	Use:   "c",
+	Short: "c is outputs the number of bytes in a file",
+	Long: `c is outputs the number of bytes in a files.
+It is a clone of the wc command.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+		if len(args) != 1 {
+			_ = fmt.Errorf("please input right file path")
+		} else {
+			fmt.Println("c called, arg:", args)
+			fileInfos, err := getFileInfo(args[0])
+			if err != nil {
+				_ = fmt.Errorf("please input right file path")
+			}
+			fmt.Println("size:" + strconv.FormatInt(fileInfos.Size(), 10))
+		}
+
+	},
+}
+
+func getFileInfo(path string) (fs.FileInfo, error) {
+	info, err := os.Stat(path)
+
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
+
+}
